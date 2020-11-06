96fa7034a0e5c3e1b1bc8c7850481fed76dbb228
ed.liu <ed.liu@bridgewell.com>
2020-11-06 14:04:53 +0800

    [doc] Add docstring

--- Parser.py
+++ Parser.py
@@ -10,15 +10,18 @@ class Parser:
         pass
 
     def _tokenize(self, s):
+        """Given a string, return a list of tokens that tokenized by jieba"""
         tokens = jieba.cut(s)
         return [token.strip() for token in tokens if token.strip()]
 
     def _clean(self, html):
+        """Given html string, remove tags and control sysmbols, and return the tokenized sentence"""
         soup = BeautifulSoup(html)
         text = soup.get_text().replace('\xa0', ' ')
         return ' '.join(self._tokenize(text))
 
     def parse(self, file_name):
+        """Given a rss file, return tokenized description (split by space)"""
         feeds = feedparser.parse(file_name).entries
         return [self._clean(feed.get('description', '')) for feed in feeds]
 
