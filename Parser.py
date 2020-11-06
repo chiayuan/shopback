8d9002eeca56ab49d9d903fd563b405573994b65
ed.liu <ed.liu@bridgewell.com>
2020-11-06 14:14:47 +0800

    [refactor] Add typing annotation

--- Parser.py
+++ Parser.py
@@ -1,26 +1,26 @@
 import jieba
 import feedparser
+from typing import List
 from bs4 import BeautifulSoup
-
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 class Parser:
     
-    def __int__(self):
+    def __int__(self) -> None:
         pass
 
-    def _tokenize(self, s):
+    def _tokenize(self, s: str) -> List[str]:
         """Given a string, return a list of tokens that tokenized by jieba"""
         tokens = jieba.cut(s)
         return [token.strip() for token in tokens if token.strip()]
 
-    def _clean(self, html):
+    def _clean(self, html: str) -> str:
         """Given html string, remove tags and control sysmbols, and return the tokenized sentence"""
         soup = BeautifulSoup(html)
         text = soup.get_text().replace('\xa0', ' ')
         return ' '.join(self._tokenize(text))
 
-    def parse(self, file_name):
+    def parse(self, file_name: str) -> List[str]:
         """Given a rss file, return tokenized description (split by space)"""
         feeds = feedparser.parse(file_name).entries
         return [self._clean(feed.get('description', '')) for feed in feeds]
