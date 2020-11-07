77b659336170e4f545df7c95357d429bc4e07dcf
chiayuan <black366678@gmail.com>
2020-11-07 15:58:13 +0800

    [feature] Add regex to remove non eng and chn words

--- Parser.py
+++ Parser.py
@@ -1,6 +1,8 @@
+import re
+from typing import List, Any
+
 import jieba
 import feedparser
-from typing import List, Any
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -16,8 +18,17 @@ class Parser:
 
     def _clean(self, html: Any) -> str:
         """Given html string, remove tags and control sysmbols, and return the tokenized sentence"""
+        # remove tags
         soup = BeautifulSoup(html)
         text = soup.get_text().replace('\xa0', ' ')
+
+        # use regex to remove meaningless chars
+        regex = r'[a-zA-Z\u4e00-\u9fff]+'
+        text = ' '.join(re.findall(regex, text))
+        
+        # to lowercase
+        text = text.lower()
+
         return ' '.join(self._tokenize(text))
 
     def parse(self, file_name: str) -> List[str]:
